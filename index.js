99025d8d896e4121f21a602924b5638ee95ff995
niranjandkar <niranjandkarakere@gmail.com>
2025-02-14 08:56:49 +0530

    second

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const app = express();
 dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "https://mern-task-app.onrender.com"
+}));
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/faculty", facultyRoutes);
